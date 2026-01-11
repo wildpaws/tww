ccf5298c333ad998f966d678a8c10559ad91cfb6
Roland <roland@wildpaws.xyz>
2026-01-11 06:04:23 -0600

    Initial work - Add g_profile and l_dMinigame_Starter_Method - Match the trivial static functions - Add static data from the object into the TU

--- include/d/d_minigame_starter.h
+++ include/d/d_minigame_starter.h
@@ -1,13 +1,14 @@
 #ifndef D_MINIGAME_STARTER_H
 #define D_MINIGAME_STARTER_H
 
+#include "d/d_drawlist.h"
 #include "dolphin/types.h"
 #include "f_op/f_op_msg.h"
 
 class JKRArchive;
 struct fopMsgM_pane_class;
 
-class dDlst_StarterScrnDraw_c {
+class dDlst_StarterScrnDraw_c : public dDlst_base_c {
 public:
     void acc(s16, s16, s16) {}
     
@@ -23,10 +24,10 @@ class dMinigame_Starter_c : public msg_class {
 public:
     void countStart() { if (!field_0x111) field_0x111 = true; }
     
-    void _create();
-    void _execute();
-    void _draw();
-    void _delete();
+    cPhs_State _create();
+    BOOL _execute();
+    BOOL _draw();
+    BOOL _delete();
     BOOL startCheck();
     BOOL deleteCheck();
 public:
--- src/d/d_minigame_starter.cpp
+++ src/d/d_minigame_starter.cpp
@@ -5,26 +5,39 @@
 
 #include "d/dolzel.h" // IWYU pragma: keep
 #include "d/d_minigame_starter.h"
+#include "d/d_priority.h"
+#include "d/d_procname.h"
 #include "f_op/f_op_msg.h"
 #include "f_op/f_op_msg_mng.h"
 
+static s16 cdFrame0 = 0x0007;
+static s16 cdFrame1 = 0x0003;
+static s16  tmFrame = 0x0005;
+static s16 cdFrame2 = 0x000F;
+static s16 cdFrame3 = 0x0005;
+static s16 cdFrame4 = 0x0007;
+static s16 cdFrame5 = 0x0003;
+static s16 cdFrame6 = 0x0004;
+static s16 cdFrame7 = 0x0004;
+static s16 cdFrame8 = 0x0006;
+
 /* 80205FE8-80206124       .text _create__19dMinigame_Starter_cFv */
-void dMinigame_Starter_c::_create() {
+cPhs_State dMinigame_Starter_c::_create() {
     /* Nonmatching */
 }
 
 /* 80206124-8020629C       .text _execute__19dMinigame_Starter_cFv */
-void dMinigame_Starter_c::_execute() {
+BOOL dMinigame_Starter_c::_execute() {
     /* Nonmatching */
 }
 
 /* 8020629C-802062DC       .text _draw__19dMinigame_Starter_cFv */
-void dMinigame_Starter_c::_draw() {
+BOOL dMinigame_Starter_c::_draw() {
     /* Nonmatching */
 }
 
 /* 802062DC-80206328       .text _delete__19dMinigame_Starter_cFv */
-void dMinigame_Starter_c::_delete() {
+BOOL dMinigame_Starter_c::_delete() {
     /* Nonmatching */
 }
 
@@ -69,26 +82,48 @@ void dDlst_StarterScrnDraw_c::draw() {
 }
 
 /* 80206EDC-80206EFC       .text dMinigame_Starter_Draw__FP19dMinigame_Starter_c */
-static BOOL dMinigame_Starter_Draw(dMinigame_Starter_c*) {
-    /* Nonmatching */
+static BOOL dMinigame_Starter_Draw(dMinigame_Starter_c* i_this) {
+    return i_this->_draw();
 }
 
 /* 80206EFC-80206F1C       .text dMinigame_Starter_Execute__FP19dMinigame_Starter_c */
-static BOOL dMinigame_Starter_Execute(dMinigame_Starter_c*) {
-    /* Nonmatching */
+static BOOL dMinigame_Starter_Execute(dMinigame_Starter_c* i_this) {
+    return i_this->_execute();
 }
 
 /* 80206F1C-80206F24       .text dMinigame_Starter_IsDelete__FP19dMinigame_Starter_c */
 static BOOL dMinigame_Starter_IsDelete(dMinigame_Starter_c*) {
-    /* Nonmatching */
+    return TRUE;
 }
 
 /* 80206F24-80206F44       .text dMinigame_Starter_Delete__FP19dMinigame_Starter_c */
-static BOOL dMinigame_Starter_Delete(dMinigame_Starter_c*) {
-    /* Nonmatching */
+static BOOL dMinigame_Starter_Delete(dMinigame_Starter_c* i_this) {
+    return i_this->_delete();
 }
 
 /* 80206F44-80206F64       .text dMinigame_Starter_Create__FP9msg_class */
-static cPhs_State dMinigame_Starter_Create(msg_class*) {
-    /* Nonmatching */
+static cPhs_State dMinigame_Starter_Create(msg_class* i_this) {
+    return ((dMinigame_Starter_c*)i_this)->_create();
 }
+
+msg_method_class l_dMinigame_Starter_Method = {
+    (process_method_func)dMinigame_Starter_Create,
+    (process_method_func)dMinigame_Starter_Delete,
+    (process_method_func)dMinigame_Starter_Execute,
+    (process_method_func)dMinigame_Starter_IsDelete,
+    (process_method_func)dMinigame_Starter_Draw,
+};
+
+msg_process_profile_definition g_profile_MINIGAME_STARTER = {
+    /* LayerID      */ fpcLy_CURRENT_e,
+    /* ListID       */ 12,
+    /* ListPrio     */ fpcPi_CURRENT_e,
+    /* ProcName     */ PROC_MINIGAME_STARTER,
+    /* Proc SubMtd  */ &g_fpcLf_Method.base,
+    /* Size         */ sizeof(dMinigame_Starter_c),
+    /* SizeOther    */ 0,
+    /* Parameters   */ 0,
+    /* Leaf SubMtd  */ &g_fopMsg_Method,
+    /* Priority     */ PRIO_MINIGAME_STARTER,
+    /* Msg SubMtd   */ &l_dMinigame_Starter_Method,
+};
